d9e6e802bd5128989fbd848dfd9e2388439879cc
victor Li <gangwei.li@outlook.com>
2018-10-07 21:33:52 +0800

    g1

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
+id=0
 pid=$$
 file=$0
 echo "id=$pid">$file
 echo "pid=\$\$" >>$file
 echo "file=\$0" >>$file
-echo "chmod +x $file" >>$file
+echo 'echo id=$pid >>\$file' >>$file
+echo 'echo pid=$$ >>\$file' >>$file
+echo 'echo file=$0 >>\$file' >>$file
+echo 'echo chmod +x $file' >>$file
+echo 'exec $file' >>$file
 chmod +x test.sh
+exec $file
